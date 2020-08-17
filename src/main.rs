d650256b2e9d71948b01e1817c11e8c2f4d7e3f8
AllowFlame <closelogin@hanmail.net>
2020-08-17 21:40:19 +0900

    unzip file test code attached and need to check performance

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod platform;
 use platform::Platform;
 
 fn main() {
-    let platform = platform();
-    let ndk_path = platform.determine_ndk_path();
-    platform.setup_config(ndk_path.as_str());
+    // let platform = platform();
+    // let ndk_path = platform.determine_ndk_path();
+    // platform.setup_config(ndk_path.as_str());
+    unzip();
 }
 
 #[cfg(target_os = "windows")]
@@ -22,4 +23,70 @@ fn platform() -> impl Platform {
 #[cfg(target_os = "linux")]
 fn platform() -> impl Platform {
     platform::LinuxConfig
+}
+
+fn unzip() {
+    real_main();
+}
+
+fn real_main() -> i32 {
+    use std::fs;
+    use std::io;
+
+    // let args: Vec<_> = std::env::args().collect();
+    // if args.len() < 2 {
+    //     println!("Usage: {} <filename>", args[0]);
+    //     return 1;
+    // }
+    // let fname = std::path::Path::new(&*args[1]);
+    let fname = std::path::Path::new("target/ndk.zip");
+    let file = fs::File::open(&fname).unwrap();
+
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = file.sanitized_name();
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {} comment: {}", i, comment);
+            }
+        }
+
+        if (&*file.name()).ends_with('/') {
+            println!(
+                "File {} extracted to \"{}\"",
+                i,
+                outpath.as_path().display()
+            );
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.as_path().display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+
+        // Get and Set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+            }
+        }
+    }
+    return 0;
 }
