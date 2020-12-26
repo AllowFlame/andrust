6f445dcd88d4df724aa56d69eab269a1f3ed6b73
AllowFlame <closelogin@hanmail.net>
2020-12-26 14:11:37 +0900

    test added

---
+++ src/unarchiver_test.rs
@@ -0,0 +1,10 @@
+use super::unarchiver;
+
+#[test]
+fn unzip_test() {
+    use std::fs;
+
+    let file_name = std::path::Path::new("zip_test/test.zip");
+    let file = fs::File::open(&file_name).unwrap();
+    unarchiver::unzip(&file);
+}
