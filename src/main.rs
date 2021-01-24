259ef394e82d50edd72fa808f475c08ff0603f54
Hoon Huh <closelogin@hanmail.net>
2021-01-24 20:41:36 +0900

    Restructuring (#17)
    * new structure has been applied
    
    * wrong usage codes are all removed

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,23 @@ mod unarchiver_test;
 fn main() {
     let platform = platform();
     let ndk_path = platform.determine_ndk_path();
+
+    //TODO: download toolset
+    let ndk_path = ndk_path.unwrap();
     platform.setup_config(ndk_path.as_str());
 }
 
 #[cfg(target_os = "windows")]
 fn platform() -> impl Platform {
-    platform::WinConfig
+    platform::WinConfig::new()
 }
 
 #[cfg(target_os = "macos")]
 fn platform() -> impl Platform {
-    platform::MacConfig
+    platform::MacConfig::new()
 }
 
 #[cfg(target_os = "linux")]
 fn platform() -> impl Platform {
-    platform::LinuxConfig
+    platform::LinuxConfig::new()
 }
